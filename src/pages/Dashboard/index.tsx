8889b048698a94f94509d069f0519d7d2ba4b367
Renan Bona <rcbp3094@gmail.com>
2021-01-02 16:48:19 -0300

    Create Dashboard html and add some css

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,8 +1,55 @@
 import React from 'react';
-import { Title } from './styles';
+import { Title, Form, Repositories } from './styles';
+import logo from '../../assets/icon-github.svg';
+import Repository from '../Repository';
+import { FiChevronRight } from 'react-icons/fi';
 
-const Dashboard: React.FC = () => (
-  <Title>Explore repositórios no Github</Title>
-);
+const Dashboard: React.FC = () => {
+  return(
+    <>
+    <img src={logo} alt="github icon"/>
+    <Title>Explore repositórios no Github</Title>
+    <Form>
+      <input placeholder="Digite Aqui"></input>
+      <button type="submit">Search</button>
+    </Form>
+
+    <Repositories>
+      <a href="#">
+        <img src="https://avatars0.githubusercontent.com/u/1902272?s=400&u=1289cb8dec14048196f079a6ac9501e9e62405c9&v=4" alt="Repository Owner Image"/>
+        <div>
+          <strong>wilkmaia/adventofcode</strong>
+          <p>Cool Description goes here!!!</p>
+        </div>
+        <FiChevronRight size={20} />
+      </a>
+      <a href="#">
+        <img src="https://avatars2.githubusercontent.com/u/30028621?s=460&u=43b301466a4f76d6b4c810e810403eacfe1f0724&v=4" alt="Repository Owner Image"/>
+        <div>
+          <strong>luchiago/dev-radar</strong>
+          <p>Cool Description goes here!!!</p>
+        </div>
+        <FiChevronRight size={20} />
+      </a>
+      <a href="#">
+        <img src="https://avatars0.githubusercontent.com/u/9610091?s=460&u=7bb359a684b0a61e41b075f2021a07ee01076059&v=4" alt="Repository Owner Image"/>
+        <div>
+          <strong>anapaulamendes/to-do-list</strong>
+          <p>Cool Description goes here!!!</p>
+        </div>
+        <FiChevronRight size={20} />
+      </a>
+      <a href="#">
+        <img src="https://avatars2.githubusercontent.com/u/8514920?s=400&u=611659b0ab608a41adc9616b298ffc971145d217&v=4" alt="Repository Owner Image"/>
+        <div>
+          <strong>marclerodrigues/headhunter42</strong>
+          <p>Cool Description goes here!!!</p>
+        </div>
+        <FiChevronRight size={20} />
+      </a>
+    </Repositories>
+    </>
+  )
+};
 
 export default Dashboard;
--- src/pages/Dashboard/styles.ts
+++ src/pages/Dashboard/styles.ts
@@ -1,6 +1,83 @@
 import styled from 'styled-components';
+import { shade } from 'polished';
 
 export const Title = styled.h1`
   font-size: 48px;
   color: #3A3A3A;
+  margin-top: 80px;
+  max-width: 450px;
+  line-height: 56px;
+`;
+
+export const Form = styled.form`
+  margin-top: 40px;
+  max-width: 700px;
+  display: flex;
+
+  input {
+    flex: 1;
+    height: 70px;
+    padding-left: 20px;
+  }
+
+  button {
+    width: 175px;
+    background: #04D361;
+    color: #FFF;
+    border-radius: 5px;
+
+    &:hover {
+      background: ${shade(0.2, '#04d361')};
+    }
+  }
+`;
+
+export const Repositories = styled.div`
+  margin-top: 60px;
+  max-width: 700px;
+
+  a {
+    display: flex;
+    background-color: #FFF;
+    padding: 24px;
+    align-items: center;
+    border-radius: 5px;
+    width: 100%;
+    text-decoration: none;
+    transition: transform 0.2s;
+
+    & + a {
+      margin-top: 15px;
+    }
+
+    &:hover {
+      transform: translateX(10px);
+    }
+
+    img {
+      width: 60px;
+      height: 60px;
+      border-radius: 50%;
+    }
+
+    div {
+      margin-left: 20px;
+
+      strong {
+        font-size: 20px;
+        color: #3d3d4d;
+      }
+
+      p {
+        font-size: 18px;
+        color: #a8a8b3;
+        margin-top: 4px;
+      }
+    }
+
+    svg {
+      margin-left: auto;
+      color: #cbcbd6;
+    }
+  }
 `;
